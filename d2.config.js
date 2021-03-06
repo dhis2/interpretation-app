8d39168a2694df61acf6be4171b1fe8ab334441b
Médi-Rémi Hashim <mediremi@gmail.com>
2021-03-06 16:31:16 +0000

    chore: add d2.config.js

---
+++ d2.config.js
@@ -0,0 +1,12 @@
+const config = {
+    type: 'app',
+    name: 'interpretation',
+    title: 'Interpretation',
+    coreApp: true,
+
+    entryPoints: {
+        app: './src/App',
+    },
+}
+
+module.exports = config
